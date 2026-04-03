49960180a2e45361b094acedd6f8843494f1e116
Darell <dlewis8605@westcliff.edu>
2026-04-02 20:41:33 -0700

    Final Week 5 Day 3 Homework Push.

--- js/activity.js
+++ js/activity.js
@@ -1,15 +1,34 @@
 $(document).ready(function() {
     // Add hand cursor to selectable activity cells
     $('tbody td:not(:first-child)').each(function() {
-        if ($(this).text() !== "Not Available") {
-            $(this).css('cursor', 'pointer');
+        if ($(this).text() !== "Not Available") { // check if content does not contain a particular string
+            $(this).css('cursor', 'pointer'); // show hand indicating selectable
         }
     });
 
-    // Toggle highlight on click for selectable cells
-    $('tbody td:not(:first-child)').click(function() {
-        if ($(this).text() !== "Not Available") {
-            $(this).toggleClass("tdhighlight");
+    // User interaction with table cells
+    $('tbody td:not(:first-child)').click(function() { // user selects a table data cell
+        var content = $(this).text(); // get content of cell
+        
+        // Find column index to get matching cliff site name
+        var colIndex = $(this).index(); // get the column index of the clicked cell
+        var cliffName = $("thead th").eq(colIndex).text(); // extract the cliff site name from the top heading row
+
+        if (content !== "Not Available") { // check if content is a valid activity
+            $(this).toggleClass("tdhighlight"); // add or remove class when cell is selected
+
+            if ($(this).hasClass("tdhighlight")) { // check if selected cell has class
+                $('#displaySelected').css("visibility", "visible"); // make display box visible
+                $('#displaySelected').css("margin-top", "2em"); // add spaces above display box
+                $('#result').append("<p>" + content + " at " + cliffName + "</p>"); // add child element with content and cliff name
+            } else { // if selected cell doesn't have class
+                $('#result p:contains("' + content + '")').remove(); // remove child element from display box
+                
+                if ($('#result').has('p').length == false) { // check if there are any child elements left within parent
+                    $('#displaySelected').css("visibility", "hidden"); // make display box hidden
+                    $('#displaySelected').css("margin-top", "0"); // remove spaces above display box
+                }
+            }
         }
     });
 });
